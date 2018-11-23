d6e90589765d5463fdf2c6d290634bfd7b68a75f
mk00984@surrey.ac.uk <mk00984@duck12.eps.surrey.ac.uk>
2018-11-23 13:00:21 +0000

    pictures on the index page are now all the same size so that it looks more presentable

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,7 +3,7 @@ class Food < ApplicationRecord
     belongs_to :user
     belongs_to :category
 
-    has_attached_file :food_image, styles: { food_index: "300x300>", food_show: "400x450>" }, default_url: "/images/:style/missing.png"
+    has_attached_file :food_image, styles: { food_index: "350x250>", food_show: "400x450>" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :food_image, content_type: /\Aimage\/.*\z/
 
 end
