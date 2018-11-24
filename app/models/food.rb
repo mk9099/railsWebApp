39ac7dea4e41bd305c1a1029a0d2c078d2d4c09e
mk00984@surrey.ac.uk <mk00984@otter77.eps.surrey.ac.uk>
2018-11-24 17:32:02 +0000

    more associations done

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,6 +2,7 @@ class Food < ApplicationRecord
 
     belongs_to :user
     belongs_to :category
+    has_many :reviews
 
     has_attached_file :food_image, styles: { food_index: "350x250>", food_show: "400x450>" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :food_image, content_type: /\Aimage\/.*\z/
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181124172038) do
+ActiveRecord::Schema.define(version: 20181124172441) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20181124172038) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.integer  "food_id"
   end
 
   create_table "users", force: :cascade do |t|
