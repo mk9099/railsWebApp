c884565d44e0688007ffa357e98620856bf49f14
mk00984@surrey.ac.uk <mk00984@otter77.eps.surrey.ac.uk>
2018-11-24 17:21:13 +0000

    user id associated to reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181122172211) do
+ActiveRecord::Schema.define(version: 20181124172038) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -34,6 +34,14 @@ ActiveRecord::Schema.define(version: 20181122172211) do
     t.string   "food_image_content_type"
     t.bigint   "food_image_file_size"
     t.datetime "food_image_updated_at"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
