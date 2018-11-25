b289cd38eb5d7aabd3116554ea66d117f3f015af
mk00984@surrey.ac.uk <mk00984@otter77.eps.surrey.ac.uk>
2018-11-25 15:15:21 +0000

    review page set up, still do make it fully functioning and presentable

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
     resources :foods do
-      resources :reivews
+      resources :reviews
     end
     root 'foods#index'
 end
