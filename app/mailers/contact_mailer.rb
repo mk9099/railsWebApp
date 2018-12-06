b5489591e1742a67f5b48fafdd897dffb8470a55
mk00984@surrey.ac.uk <mk00984@penguin17.eps.surrey.ac.uk>
2018-12-06 16:56:06 +0000

    mailer view

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,2 +1,13 @@
 class ContactMailer < ApplicationMailer
+
+  def contact_email(email, name, telephone, message)
+    @email = email
+    @name = name
+    @telephone = telephone
+    @message = message
+
+    mail cc: @email
+    
+  end
+
 end
